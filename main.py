cc1040bc70830814fcdf221a8cf18a662336d8fa
jpnykw <jpnykw@gmail.com>
2021-06-21 18:51:30 +0900

    :recycle: Support for corrupt files and opaque images

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ from matplotlib import pyplot as plt
 debug = False
 
 # 分割数（数値を下げるほど細分化するため処理が遅くなる）
-step = 4
+step = 5
 
 # samples 列のヒストグラムの平均を使用する
-samples = 5
+samples = 2
 
 # 検知位置からサンプル量に合わせて左側にずらすピクセル数
 diff_x = step * samples
@@ -22,9 +22,9 @@ classifier = cv2.CascadeClassifier('./lbpcascade_animeface.xml')
 
 def calc_hist_levels(image, ceil):
     b, g, r = image[:, :, 0], image[:, :, 1], image[:, :, 2]
-    hist_r = cv2.calcHist([r], [0], None, [256], [0,256])
-    hist_g = cv2.calcHist([g], [0], None, [256], [0,256])
-    hist_b = cv2.calcHist([b], [0], None, [256], [0,256])
+    hist_r = cv2.calcHist([r], [0], None, [256], [0, 256])
+    hist_g = cv2.calcHist([g], [0], None, [256], [0, 256])
+    hist_b = cv2.calcHist([b], [0], None, [256], [0, 256])
     return sum([ceil - hist.max() for hist in [hist_r, hist_g, hist_b]])
 
 # TODO: ユーザーからの入力を受け取ってパスを設定する
@@ -59,10 +59,19 @@ for file_id in range(0, length):
 
         if file_type == 'png':
             base_image = cv2.imread('{}{}.{}'.format(target_path, file_name, file_type), -1)
+
+            # 読み込んだ画像が破損していたら continue する
+            if base_image is None:
+                print('{} is broken'.format(file_name))
+                continue
+
             height, width = base_image.shape[:2]
+
             # 背景が透過されている場合は白くする
-            index = np.where(base_image[:, :, 3] == 0)
-            base_image[index] = [255, 255, 255, 255]
+            # RGB でも透過ピクセルが存在しない可能性もあるので shape を確認する
+            if base_image.shape[2] == 4:
+                index = np.where(base_image[:, :, 3] == 0)
+                base_image[index] = [255, 255, 255, 255]
 
             # 透過箇所を白塗りした png を一時的に jpg 変換して保存
             cv2.imwrite('tmp.jpg', base_image, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
@@ -91,7 +100,7 @@ for file_id in range(0, length):
         # print(width, height)
 
         # しきい値を計算
-        ht = width * 0.05
+        ht = width * -2.58
 
         # ヒストグラムのレベルを保存する
         horizontal_levels = []
@@ -105,30 +114,40 @@ for file_id in range(0, length):
         person_count = 0
         crop_start_y = 0
 
-        print('\033[32m', 'step =', step, ', (h)t =', ht, ', width =', width, ', height =', height, '\033[0m')
+        h_step = 5
+        h_samples = 2
+
+        print('\033[32m', '(h)step =', h_step, ', (h)t =', ht, ', width =', width, ', height =', height, '\033[0m')
 
         # 複数列になっている場合があるので配列にする
         images = []
 
         # 複数列になっている場合を想定して上から横方向にスライスして同様にキャラの範囲を判定
-        for y in range(0, height, step):
+        for y in range(0, height, h_step):
             # 水平方向
-            trimmed_image = image[y : y + step, 0 : width]
+            trimmed_image = image[y : y + h_step, 0 : width]
             horizontal_levels.append(calc_hist_levels(trimmed_image, width * 3))
 
-            if len(horizontal_levels) > samples:
+            # print('t:', ht, 'acc:', horizontal_levels[len(horizontal_levels) - 1])
+            # cv2.imshow('trim', trimmed_image)
+            # cv2.waitKey(0)
+
+            if len(horizontal_levels) > h_samples:
                 horizontal_levels.pop(0)
-                level_sum = sum(horizontal_levels) / samples
+                level_sum = sum(horizontal_levels) / h_samples
 
                 if (not crop_flag and ht < level_sum) or (crop_flag and ht > level_sum):
                     crop_flag = not crop_flag
 
                     if crop_flag:
-                        crop_start_y = y - step * samples
+                        crop_start_y = y - h_step * h_samples
                     else:
-                        if width / (y - crop_start_y) > 10: continue
-                        line_image = image[crop_start_y - diff_x : y, 0 : width]
+                        if width / (y - crop_start_y) > 5: continue
+                        line_image = image[crop_start_y - h_step * h_samples : y, 0 : width]
                         images.append(line_image)
+
+                        # cv2.imshow('line image', line_image)
+                        # cv2.waitKey(0)
 
         print('\033[32m', 'vertical images', len(images), '\033[0m')
 
@@ -141,7 +160,7 @@ for file_id in range(0, length):
             height, width = image.shape[:2]
             width += margin_width * 2 # 左右に余白がある分を加算
             height += margin_height * 2 # 上下に余白がある分を加算
-            vt = height * 1.23 # キャラクターの境界を示すしきい値
+            vt = height * -0.65 # キャラクターの境界を示すしきい値
             
             print('\033[32m', 'step =', step, ', (v)t =', vt, ', width =', width, ', height =', height, '\033[0m')
 
@@ -162,9 +181,9 @@ for file_id in range(0, length):
                         if crop_flag:
                             crop_start_x = x
                         else:
-                            # あまりに縦長の画像を弾くためにアスペクト比が 1:5 を超える画像は無視する
+                            # あまりに縦長の画像を弾くためにアスペクト比が 1:7 を超える画像は無視する
                             # 髪の毛などを人として誤検知してしまった場合に必要な判定
-                            if height / (x - crop_start_x) > 5: continue
+                            if height / (x - crop_start_x) > 7 : continue
 
                             person_count += 1
                             person = image[0 : height, crop_start_x - diff_x : x]
@@ -199,9 +218,17 @@ for file_id in range(0, length):
                                     for _ in range(0, faces_count):
                                         person_count += 1
                                         person = image[0 : height, x_area : x_area + w_area]
-                                        new_file_path = '{}{}_{}.{}'.format(output_path, file_name, person_count, file_type)
-                                        print(' - New person detected; save in {}'.format(new_file_path))
-                                        cv2.imwrite(new_file_path, person)
+
+                                        # 分割位置がズレていた場合に再計算する
+                                        gray_image = cv2.cvtColor(person, cv2.COLOR_BGR2GRAY)
+                                        faces = classifier.detectMultiScale(gray_image)
+
+                                        # 検出した場合だけ保存する
+                                        if len(faces) > 0:
+                                            new_file_path = '{}{}_{}.{}'.format(output_path, file_name, person_count, file_type)
+                                            print(' - New person detected; save in {}'.format(new_file_path))
+                                            cv2.imwrite(new_file_path, person)
+
                                         x_area += w_area
                             else:
                                 print(' - New person detected; save in {}'.format(new_file_path))
@@ -209,7 +236,6 @@ for file_id in range(0, length):
 
                 if debug:
                     debug_image = image.copy()
-                    # cv2.rectangle(debug_image, start_point, end_point, (0, 180, 0), 1)
 
                     debug_width = round(width / 2)
                     debug_height = round(height / 2)
@@ -219,9 +245,8 @@ for file_id in range(0, length):
                     cv2.moveWindow('image', 800, 150)
 
                     plt.title('x = {} (step = {})'.format(x, step))
-                    # plt.savefig('./images/plots/{}_{}.jpg'.format(x, step))
                     plt.show()
-                    exit()
+                    # exit()
     else:
         print('Unsupported file types *.{}'.format(file_type))
 
